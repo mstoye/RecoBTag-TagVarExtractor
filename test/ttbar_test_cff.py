736e13b90a143b64f7134da17510528b70e75729
Markus <markus.stoye@cern.ch>
2016-10-03 16:42:58 +0200

    the test sample

---
+++ test/ttbar_test_cff.py
@@ -0,0 +1,12 @@
+inputFiles = [
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_91.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_92.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_93.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_94.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_95.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_96.root',  
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_97.root',  
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_98.root', 
+'root://eoscms.cern.ch//eos/cms/store/cmst3/user/mstoye/CSVv2Tuples/TTbar/JetTree_mc_99.root', 
+]
+
